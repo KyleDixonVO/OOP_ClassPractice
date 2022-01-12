60c14591fd24552d984036557201ac31c30f06fb
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2022-01-12 09:41:02 -0400

    Created Project Files

---
+++ OOP_ClassPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_ClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
